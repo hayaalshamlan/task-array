b1b3eb19e1428e4c3af7813a37ef2aedfa69f004
Haya A S Sh M Alshamlan <83336@kfh.com>
2024-01-03 10:30:00 +0300

    save my file

---
+++ Main.java
@@ -0,0 +1,26 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class Main {
+    public static void main(String[] args) {
+      
+      int[] temperatures = {10,20,30,40,50};
+      temperatures[2] = 19;
+      for(int temperature : temperatures ){
+        System.out.println(temperature);
+      }
+      
+       
+      // System.out.println("display all elements:");
+      // for(int temperature : temperatures){
+      //   System.out.println(temperature);
+        
+      }
+    }
+  
+    // @Test
+    // void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
+  
